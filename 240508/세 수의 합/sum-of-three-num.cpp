9b1356da963862ef06a825f0ba6e12df1091a6c7
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-05-08 18:09:17 +0900

    Title: 세 수의 합, Time: 188ms, Memory: 0MB, Status: Passed - CodeTree

--- 240508/세 수의 합/sum-of-three-num.cpp
+++ 240508/세 수의 합/sum-of-three-num.cpp
@@ -1,48 +1,29 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
-#include <utility>
-#include <algorithm>
-typedef long long ll;
 
 using namespace std;
 
-unordered_map<ll,ll> um;
-
-//두개의 수를 더해서 나오는 경우의 수 배열을 만들고
-//
-
-ll n,k;
-ll answer = 0;
-vector<ll> v;
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int n, k;
     cin >> n >> k;
-    ll N = n;
-    while(n--){
-        int n1;
-        cin >> n1;
-        um[n1]++;
-        v.push_back(n1);
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
     }
 
-    vector<ll> per;
+    int count = 0;
 
-    for(int i = 0;i<v.size();i++){
-        per.push_back(1);
-    }
-
-    per[0] = 0;
-    per[1] = 0;
-    per[2] = 0;
-
-    do{
-        ll ans  =0;
-        for(int i =0;i<per.size();i++){
-            if(per[i] == 0) ans += v[i];
+    // 세 개의 서로 다른 위치를 모두 조합하는 반복문
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            for (int m = j + 1; m < n; ++m) {
+                if (nums[i] + nums[j] + nums[m] == k) {
+                    count++;
+                }
+            }
         }
-        if(ans == k) answer++;
-    }while(next_permutation(per.begin(), per.end()));
-    cout << answer;
+    }
+
+    cout << count << endl;
     return 0;
 }
