53f7ba820fe22c0ce83a55dd80faa8096b5fe339
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-05-08 18:15:07 +0900

    Title: 자주 등장한 top k 숫자, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240508/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+int n, k;
+
+using namespace std;
+int arr[100010];
+unordered_map<int,int> um;
+vector<pair<int,int>> v;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> k;
+
+    while(n--){
+        int n1;
+        cin >> n1;
+        arr[n1]++;
+    }
+
+    for(int i =0;i<100010;i++){
+        if(arr[i] != 0)v.push_back({i,arr[i]});
+    }
+
+    sort(v.begin(), v.end(), [](const auto&a, const auto&b){
+        if(a.second!=b.second) return a.second > b.second;
+        return a.first > b.first;
+    });
+
+    for(int i =0;i<v.size();i++){
+        if(v[i].second >= k) cout << v[i].first << " " ;
+    }
+    
+    return 0;
+}
