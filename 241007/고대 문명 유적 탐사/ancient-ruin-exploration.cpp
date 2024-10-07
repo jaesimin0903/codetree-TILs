d1a385ca25e3b565800b16d3f5381b970c9a265f
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-10-07 22:55:08 +0900

    Title: 고대 문명 유적 탐사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241007/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
+++ 241007/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
@@ -169,8 +169,8 @@ void rotate(pair<int, int> p, int deg) {
 pair<pair<int, int>, int> findRotateCell() {
     pair<pair<int, int>, int> result = { {-1,-1}, -1 };
     int maxPart = 0;
-    for (int j = 4; j >= 0;j--) {
-        for (int i = 4; i >= 0;i--) {
+    for (int j = 3; j >= 1;j--) {
+        for (int i = 3; i >= 1;i--) {
             for (int r = 0; r < 3; r++) {
                 rotate(make_pair(i, j), r);
                 int part = bfs(false);
